ed62b8584177bc14ed9c6f9c7807d10af220d096
William Reithmeyer <billreithmeyer@gmail.com>
2016-02-09 11:51:58 -0500

    Update TwitchBot.java

--- TwitchBot.java
+++ TwitchBot.java
@@ -1,3 +1,9 @@
+/*
+    TwitchBot using PIRCBOT
+    Created by William R.
+    
+*/
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -5,27 +11,31 @@ import java.util.Scanner;
 import org.jibble.pircbot.*;
 
 public class TwitchBot extends PircBot{
+    // initiate bot
     static TwitchBot bot = new TwitchBot();
     
+    // channel settings
     static final String MY_CHANNEL = "";
     static final String MY_NAME = "";
     static final String BOT_NAME = "";
     static final String TWITCH_IRC = "irc.twitch.tv";
     static final String TWITCH_OAUTH = "";
     static final int    TWITCH_PORT = 6667;
-    static final String FILE_LOCATION = "questions.txt";
     
-    static ArrayList<String> questionArray = new ArrayList<String>();
+    // question settings
+    static final String FILE_LOCATION = "questions.txt";
+    //
+    static ArrayList<String> questionArray = new ArrayList();
     static String currentQuestion = "";
     static double currentQuestionPosition = -1;
 
     public static void main(String[] args) throws Exception {
         
         // Enable debugging output.
-        bot.setVerbose(true);
+//        bot.setVerbose(true);
         
-        // Connect to the IRC server.
-        // to prevent duplication
+        // Connect
+        // prevent duplication
         try {
             bot.connect(TWITCH_IRC, TWITCH_PORT, TWITCH_OAUTH);
         } catch (NickAlreadyInUseException e) {
@@ -35,15 +45,19 @@ public class TwitchBot extends PircBot{
             e.printStackTrace();
         }
 
-        // Join the #pircbot channel.
+        // Join the channel.
         bot.joinChannel(MY_CHANNEL);
         
         // Get the questions from the file
         _getQuestions(FILE_LOCATION);
     }
+    
+    
     public TwitchBot(){
         this.setName(BOT_NAME);
     }
+    
+    
     @Override
     public void onMessage(String channel, String sender,
                        String login, String hostname, String message) {
@@ -51,25 +65,22 @@ public class TwitchBot extends PircBot{
         String command = messageArray[0];
 
         if (sender.equalsIgnoreCase(MY_NAME)){
-            // Commands only for Rithek
+            // Commands only for "MY_NAME"
+            // should be host
             switch(command){
                 case "!question":
                     // display the next question
                     sendMessage(channel, _getNextQuestion());
                     return;
-                case "":
-                    
-                    return;
                 default:break;
 
             }
         }
-        // commands for everyone
+        // commands for everyone in chat
         switch(command){
             case "!currentquestion":
                 // display the current question
                 sendMessage(channel, _getCurrentQuestion());
-                return;
             default:break;
 
         }
@@ -80,11 +91,15 @@ public class TwitchBot extends PircBot{
     */
     private static void _getQuestions(String file) throws FileNotFoundException{
         
-        Scanner inFile = new Scanner(new File(FILE_LOCATION));
-        while (inFile.hasNextLine()){
-            questionArray.add(inFile.next());
+        // Try to read the file.
+        // Cycle through each line, adding it to the array
+        try (Scanner inFile = new Scanner(new File(FILE_LOCATION))) {
+            while (inFile.hasNextLine()){
+                questionArray.add(inFile.next());
+            }
+        } catch (FileNotFoundException e){
+            System.err.println("Can't find the file: " + file);
         }
-        inFile.close();
     }
     /*
         _getNextQuestion
@@ -95,12 +110,14 @@ public class TwitchBot extends PircBot{
     */
     private String _getNextQuestion(){
         
-        // remove the last question from array
+        // remove the last question used from array
         if (currentQuestionPosition >= 0 ) {
             questionArray.remove(currentQuestionPosition);
         }
         // get a new random position
-        int randomNumber = Integer.parseInt(Math.floor(Math.random() * questionArray.size()) + "");
+        int randomNumber = Integer.parseInt(
+                Math.floor(Math.random() * questionArray.size()) + "");
+        // grab question with random number ^
         String randomQuestion = questionArray.get(randomNumber);
         
         currentQuestion = randomQuestion;
@@ -114,8 +131,8 @@ public class TwitchBot extends PircBot{
     /*
         _getCurrentQuestion
         Return the current question.
-        // Needs better handling for spam
-        // add a delay **
+        // TODO - Needs better handling for spam
+        //      - add a delay **
     */
     private String _getCurrentQuestion(){
         
